80bd6349e40244d1a2355999509847644ae2c98a
kim-hyeonji <myfavworkofart@gmail.com>
2022-01-23 15:57:45 +0100

    new navbar

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+const toggleBtn2 = document.querySelector('.iconLink');
+const menu2 = document.querySelector('.pagesMenuContainer');
+const icons2 = document.querySelector('.snsMenuContainer');
+
+toggleBtn2.addEventListener('click', () => {
+    menu2.classList.toggle('active');
+    icons2.classList.toggle('active');
+});
