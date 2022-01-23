c8a8123202698c48f3b1d00bbac3f5b3b884098d
kim-hyeonji <myfavworkofart@gmail.com>
2022-01-23 16:45:36 +0100

    navbar working

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
-
 const toggleBtn2 = document.querySelector('.iconLink');
 const menu2 = document.querySelector('.pagesMenuContainer');
 const icons2 = document.querySelector('.snsMenuContainer');
