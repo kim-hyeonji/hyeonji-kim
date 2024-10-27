a6254894fba10c6cba4be2affd6516aa82a41b5a
Hyeonji Kim <myfavworkofart@gmail.com>
2024-10-27 19:33:19 +0100

    nav bar styling first iteration

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn2 = document.querySelector('.iconLink');
-const menu2 = document.querySelector('.pagesMenuContainer');
-const icons2 = document.querySelector('.snsMenuContainer');
-
-toggleBtn2.addEventListener('click', () => {
-    menu2.classList.toggle('active');
-    icons2.classList.toggle('active');
-});
